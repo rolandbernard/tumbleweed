cd167b503b9c11a93cb4ffdc2da95506694898a8
Roland Bernard <rolli.bernard@gmail.com>
2020-09-20 20:42:50 +0200

    Work on codegen

--- src/codegen/casts.c
+++ src/codegen/casts.c
@@ -21,6 +21,21 @@ LLVMValueRef generateCastFromTo(Ast* ast, LLVMValueRef value, LLVMTypeRef dest, 
 }
 
 LLVMValueRef generateConstCastFromTo(Ast* ast, LLVMValueRef value, LLVMTypeRef dest, Args* args, SymbolTable* symbols, ErrorContext* error_context) {
+    LLVMTypeRef src = LLVMTypeOf(value);
+    if(src == dest) {
+        return value;
+    } else if(LLVMGetTypeKind(src) == LLVMGetTypeKind(dest)) {
+        if(LLVMGetTypeKind(src) == LLVMPointerTypeKind) {
+            return LLVMConstPointerCast(value, dest);
+        } else if(LLVMGetTypeKind(src) == LLVMIntegerTypeKind) {
+            return LLVMConstSExt(value, dest);
+        } else if(LLVMGetTypeKind(src) == LLVMFloatTypeKind || LLVMGetTypeKind(src) == LLVMDoubleTypeKind || 
+            LLVMGetTypeKind(src) == LLVMHalfTypeKind || LLVMGetTypeKind(src) == LLVMX86_FP80TypeKind ||
+            LLVMGetTypeKind(src) == LLVMFP128TypeKind) {
+            return LLVMConstFPCast(value, dest);
+        }
+    }
+    addError(error_context, "Unable to cast this value", ast->start, ERROR);
     return NULL;
 }
 
--- src/codegen/operations.c
+++ src/codegen/operations.c
@@ -1,6 +1,15 @@
 
 #include "codegen/operations.h"
 #include "codegen/general.h"
+#include "codegen/casts.h"
+
+int llvm_float_type_to_length[] = {
+    [LLVMHalfTypeKind] = 16,
+    [LLVMFloatTypeKind] = 32,
+    [LLVMDoubleTypeKind] = 64,
+    [LLVMX86_FP80TypeKind] = 80,
+    [LLVMFP128TypeKind] = 128,
+};
 
 LLVMValueRef generateValueLazyAnd(AstBinaryOperation* ast, Symbol* function, LLVMBuilderRef builder, Args* args, SymbolTable* symbols, ErrorContext* error_context) {
     return NULL;
@@ -55,6 +64,65 @@ LLVMValueRef generateValueShiftLeft(AstBinaryOperation* ast, Symbol* function, L
 }
 
 LLVMValueRef generateValueAdd(AstBinaryOperation* ast, Symbol* function, LLVMBuilderRef builder, Args* args, SymbolTable* symbols, ErrorContext* error_context) {
+    bool error = false;
+    LLVMValueRef left = generateValueInFunction(ast->lhs, function, builder, args, symbols, error_context);
+    if(left == NULL) {
+        error = true;
+    } else {
+        left = generateExtractFromVariable(left, function, builder, args, symbols, error_context);
+        if (LLVMGetTypeKind((LLVMTypeOf(left))) != LLVMIntegerTypeKind &&
+            LLVMGetTypeKind((LLVMTypeOf(left))) != LLVMHalfTypeKind &&
+            LLVMGetTypeKind((LLVMTypeOf(left))) != LLVMFloatTypeKind &&
+            LLVMGetTypeKind((LLVMTypeOf(left))) != LLVMDoubleTypeKind &&
+            LLVMGetTypeKind((LLVMTypeOf(left))) != LLVMX86_FP80TypeKind &&
+            LLVMGetTypeKind((LLVMTypeOf(left))) != LLVMFP128TypeKind) {            
+            addError(error_context, "The add operation only works for integers and floats", ast->lhs->start, ERROR);
+            error = true;
+        }
+    }
+    LLVMValueRef right = generateValueInFunction(ast->rhs, function, builder, args, symbols, error_context);
+    if(right == NULL) {
+        error = true;
+    } else {
+        right = generateExtractFromVariable(right, function, builder, args, symbols, error_context);
+        if (LLVMGetTypeKind((LLVMTypeOf(right))) != LLVMIntegerTypeKind &&
+            LLVMGetTypeKind((LLVMTypeOf(right))) != LLVMHalfTypeKind &&
+            LLVMGetTypeKind((LLVMTypeOf(right))) != LLVMFloatTypeKind &&
+            LLVMGetTypeKind((LLVMTypeOf(right))) != LLVMDoubleTypeKind &&
+            LLVMGetTypeKind((LLVMTypeOf(right))) != LLVMX86_FP80TypeKind &&
+            LLVMGetTypeKind((LLVMTypeOf(right))) != LLVMFP128TypeKind) {            
+            addError(error_context, "The add operation only works for integers and floats", ast->rhs->start, ERROR);
+            error = true;
+        }
+    }
+    if(!error) {
+        if(LLVMTypeOf(left) != LLVMTypeOf(right)) {
+            if (LLVMGetTypeKind(LLVMTypeOf(right)) != LLVMIntegerTypeKind && LLVMGetTypeKind(LLVMTypeOf(left)) == LLVMIntegerTypeKind) {
+                left = generateCastFromTo(ast->lhs, left, LLVMTypeOf(right), function, builder, args, symbols, error_context);
+            } else if (LLVMGetTypeKind(LLVMTypeOf(left)) != LLVMIntegerTypeKind && LLVMGetTypeKind(LLVMTypeOf(right)) == LLVMIntegerTypeKind) {
+                right = generateCastFromTo(ast->rhs, right, LLVMTypeOf(left), function, builder, args, symbols, error_context);
+            } else if (LLVMGetTypeKind(LLVMTypeOf(left)) == LLVMIntegerTypeKind) {
+                if (LLVMGetIntTypeWidth(LLVMTypeOf(left)) > LLVMGetIntTypeWidth(LLVMTypeOf(right))) {
+                    right = generateCastFromTo(ast->rhs, right, LLVMTypeOf(left), function, builder, args, symbols, error_context);
+                } else {
+                    left = generateCastFromTo(ast->lhs, left, LLVMTypeOf(right), function, builder, args, symbols, error_context);
+                }
+            } else {
+                if (llvm_float_type_to_length[LLVMGetTypeKind(LLVMTypeOf(left))] > llvm_float_type_to_length[LLVMGetTypeKind(LLVMTypeOf(right))]) {
+                    right = generateCastFromTo(ast->rhs, right, LLVMTypeOf(left), function, builder, args, symbols, error_context);
+                } else {
+                    left = generateCastFromTo(ast->lhs, left, LLVMTypeOf(right), function, builder, args, symbols, error_context);
+                }
+            }
+        }
+        if(!error) {
+            if(LLVMGetTypeKind((LLVMTypeOf(right))) == LLVMIntegerTypeKind) {
+                return LLVMBuildAdd(builder, left, right, "");
+            } else {
+                return LLVMBuildFAdd(builder, left, right, "");
+            }
+        }
+    }
     return NULL;
 }
 
